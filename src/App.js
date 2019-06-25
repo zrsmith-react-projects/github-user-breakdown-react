19776871cd7127359e109013eaf921f28ed16f3d
Zac Smith <zrs3141592@gmail.com>
2019-06-25 00:07:38 -0600

    build navbar component and style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h2>Test from App</h2>
     </div>
   );
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./navbar.scss";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar">
+        <a href="/">github user breakdown</a>
+      </div>
+    );
+  }
+}
